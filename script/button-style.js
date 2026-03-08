6eec288f6a2a075bb16b2e06f69852867022dfa2
MAlmamunislam <almamun2026islam@gmail.com>
2026-03-09 00:17:41 +0600

    filal commit

--- script/button-style.js
+++ script/button-style.js
@@ -4,18 +4,18 @@ const buttonStyle = (btnName) => {
      const closedButton = document.getElementById('closedbutton');
      if(btnName === 'All'){
           allButton.classList.add('btn-primary')
-          openButton.classList.remove('btn-primary')
-          closedButton.classList.remove('btn-primary')
+          openButton.classList.remove('btn-success' ,'text-white')
+          closedButton.classList.remove('btn-error' ,'text-white')
      }
      else if (btnName === 'Open'){
           allButton.classList.remove('btn-primary')
-          openButton.classList.add('btn-primary')
-          closedButton.classList.remove('btn-primary')
+          openButton.classList.add('btn-success' ,'text-white')
+          closedButton.classList.remove('btn-error' ,'text-white')
 
      }
      else if (btnName === 'Closed'){
            allButton.classList.remove('btn-primary')
-          openButton.classList.remove('btn-primary')
-          closedButton.classList.add('btn-primary')
+          openButton.classList.remove('btn-success' ,'text-white')
+          closedButton.classList.add('btn-error' ,'text-white')
      }
 }
